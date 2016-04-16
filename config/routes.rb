e6a4be88c4ee526eaa6534055209f925748284b3
humbe1014 <xpedientnetechservices@gmail.com>
2016-04-16 01:30:22 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root "welcome#index"
+  root 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"
 
   # Example of regular route:
